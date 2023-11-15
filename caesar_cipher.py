cda575ba48c2c9c60ff73574cd1bf9d2b93c9a95
MihanEGGS <125635739+MihanEGGS@users.noreply.github.com>
2023-11-15 13:02:12 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,22 +1,22 @@
 a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def e(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
-    else:
-      r += (a[(a.find(c) + o) % len(a)])
-  return r
+def encrypt(text, o):#Функция для зашифровки
+  result = "" 
+  for char in text:# цикл, что перебирает символы в тексте, пока не дойдет до конца текста  
+    if (alph.find(char) == -1):#если символ не найден в алфавите , то возвращается -1
+      result += char
+    else:# если таки найден, то он добавляется к алфавиту
+      result += (alph[(alph.find(char) + o) % len(alph)])
+  return result
 
-def d(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
-    else:
-      r += (a[(a.find(c) - o) % len(a)])
-  return r
+def decrypt(text, o):# функция для расшифровки
+  result = ""
+  for char in text:# цикл, что перебирает символы в тексте, пока не дойдет до конца текста  
+    if (alph.find(char) == -1):#если символ не найден в алфавите , то возвращается -1
+      result += char
+    else:# если таки найден, то он добавляется к алфавиту
+      result += (alph[(alph.find(char) - o) % len(alph)])
+  return result
 
 w = """
 1. Encrypt text
@@ -25,14 +25,15 @@ w = """
 Choose mode: """
 mode = int(input(w))
 
-if mode == 1:
+if mode == 1:#Если пользователь хочет зашифровать текст
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Encrypted: " + e(text, rotation))
-elif mode == 2:
+  print("Encrypted: " + encrypt(text, rotation))
+elif mode == 2:#Если пользователь хочет расшифровать текст 
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Decrypted: " + d(text, rotation))
-elif mode == 3:
+  print("Decrypted: " + decrypt(text, rotation))
+elif mode == 3:#Если нужно затроллить пользователя
   print("Bruteforcing...")
   print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
+
